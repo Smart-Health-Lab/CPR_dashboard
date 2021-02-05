3cf7c85b079ba7f9a2c9be8308b0becbefb756cc
yeejun007 <dlwns007@gmail.com>
2021-02-05 13:00:08 +0900

    bodyimage height fixed & change index.html's title

--- client/src/components/BodyImage.js
+++ client/src/components/BodyImage.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import bodyImage from "../data/screenShot.png";
 
 class BodyImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <img
