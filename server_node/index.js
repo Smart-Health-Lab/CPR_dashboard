4dde43f2a40783a0bbdd41ceeae1ac5bb207ce36
yeejun007 <dlwns007@gmail.com>
2021-02-08 17:09:16 +0900

    node server cors allow complete

--- server_node/index.js
+++ server_node/index.js
@@ -2,11 +2,25 @@ const app = require("express")();
 
 const cors = require("cors");
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+//미들웨어로 cors 허용하는거는 작동 안함
+app.use(cors(corsOptions));
 
 const http = require("http").createServer(app);
 
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
+http.listen(3002, () => {
+  console.log("Connected at 3002");
+});
 
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname + "/index.html");
@@ -21,8 +35,4 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-});
-
-http.listen(3002, () => {
-  console.log("Connected at 3002");
 });
