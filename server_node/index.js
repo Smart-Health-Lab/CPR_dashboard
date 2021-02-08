05ec1b5c81b345dc06a15d45f8b1b89e52aeae23
yeejun007 <dlwns007@gmail.com>
2021-02-08 16:54:13 +0900

    create socket node server

--- server_node/index.js
+++ server_node/index.js
@@ -8,9 +8,9 @@ const http = require("http").createServer(app);
 
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
